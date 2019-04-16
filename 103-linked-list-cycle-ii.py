bdeb9b808c5b8ce2c215721efb3c18143ae03296
chenbensheng <chenbensheng@vipkid.com.cn>
2019-04-16 19:12:41 +0800

    判断是否带环链表

---
+++ 103-linked-list-cycle-ii.py
@@ -0,0 +1,19 @@
+class Cycle:
+
+
+    def detectCycle(self,head) :
+        if head ==None or head.next==None :
+            return None
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if fast==slow:
+                break
+        if slow==fast:
+            slow=head
+            while slow!=fast :
+                slow=slow.next
+                fast=fast.next
+            return slow
+        return None
