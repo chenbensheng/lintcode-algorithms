ea9443c816fc7e525021dae661718bb539ed1689
chenbensheng <chenbensheng@vipkid.com.cn>
2019-04-16 12:02:07 +0800

    reverse_list test

---
+++ 35-reverse-linked-list.py
@@ -0,0 +1,46 @@
+class Node :
+    def __init__(self,elem,next=None):
+        self.elem = elem
+        self.next = next
+
+def reverse_list(pHead):
+    if not pHead or not pHead.next:
+        return pHead
+
+    newHead =None
+    while pHead:
+        temp=pHead.next
+        pHead.next=newHead
+        newHead=pHead
+        pHead=temp
+    return newHead
+
+
+def insert_node(phead, elem):
+    node = Node(elem)
+    if phead == None:
+        phead = node
+    else:
+        p = phead
+        while p.next:
+            p = p.next
+        p.next = node
+    return phead
+
+
+if __name__ == '__main__':
+    phead = None
+    for i in range(10):
+        phead = insert_node(phead, i)
+    p = phead
+
+    while p:
+        print(p.elem, end=" ")
+        p = p.next
+
+    phead = reverse_list(phead)
+    p = phead
+    print("")
+    while p:
+        print(p.elem, end=" ")
+        p = p.next
